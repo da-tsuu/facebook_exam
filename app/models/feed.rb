7dbccff501a49531edfb7050ce04a73fbe713444
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-10-15 08:19:44 +0000

    画像投稿機能を実装し、不要だったpostテーブルを削除

---
+++ app/models/feed.rb
@@ -0,0 +1,6 @@
+class Feed < ApplicationRecord
+  belongs_to :user
+  mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :content, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  root "feeds#index"
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  resources :posts
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
 end
